bc244da7e7064d83d63118ab468c8e1f74691db1
PB-ux <bratkov.pasha21@gmail.com>
2022-04-23 03:32:35 +0300

    completion of development

---
+++ CounterDownTimer/script.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const myNewYear = "1 Jan 2023";
+
+function counterdownTimer() {
+  const newYear = new Date(myNewYear);
+  const currentYear = new Date();
+
+  const totalSeconds = (newYear - currentYear) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
+}
+
+// inital call
+counterdownTimer();
+
+setInterval(counterdownTimer, 1000);
