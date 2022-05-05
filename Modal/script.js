c2beb7e462ca00a4a641eec742e94e835a4994c9
PB-ux <bratkov.pasha21@gmail.com>
2022-05-05 18:18:57 +0300

    review code

--- Modal/script.js
+++ Modal/script.js
@@ -1,14 +1,9 @@
-const body = document.body;
 const modal = document.querySelector(".modal");
-const openModalBtn = document.querySelector(".box-modal-btn");
-const closeModalBtn = document.querySelector(".modal-btn-close");
 
-openModalBtn.addEventListener("click", () => {
-  body.classList.add("bg_blur");
-  modal.classList.add("closed");
-});
+function openModal() {
+  modal.classList.add("open");
+}
 
-closeModalBtn.addEventListener("click", () => {
-  body.classList.remove("bg_blur");
-  modal.classList.remove("closed");
-});
+function closeModal() {
+  modal.classList.remove("open");
+}
