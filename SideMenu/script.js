8962e7993a2711c5ab2ba37b4e015eddf1d32d2b
PB-ux <bratkov.pasha21@gmail.com>
2022-05-26 20:46:59 +0300

    finished project side menu

--- SideMenu/script.js
+++ SideMenu/script.js
@@ -1,1 +1,30 @@
-console.log("hello");
+// Buttons
+const containerBtn = document.querySelector(".container-btn");
+const btnSearch = document.querySelector(".btn-search");
+const btnRun = document.querySelector(".btn-run");
+const btnClose = document.querySelector(".btn-close");
+
+// Side menu
+const sideMenu = document.querySelector(".container-side-menu");
+const sideTitle = document.querySelector(".content__title");
+
+// Black layer
+const blackLayer = document.querySelector(".black-layer");
+
+// Events
+function show(title, btn) {
+  btnClose.innerHTML = btn;
+  sideTitle.innerHTML = title;
+  sideMenu.style.left = "0";
+  blackLayer.style.opacity = "0.65";
+  blackLayer.style.zIndex = "1";
+}
+
+function close() {
+  sideMenu.style.left = "-500px";
+  blackLayer.style.opacity = "0";
+  blackLayer.style.zIndex = "-1";
+}
+
+btnClose.addEventListener("click", close);
+blackLayer.addEventListener("click", close);
